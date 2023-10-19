3dbef965d422a6a22e7fe2933002fdd0d227ebb4
GVMartin23 <MartinGV19@gcc.edu>
2023-10-19 12:20:21 -0400

    Basic Start Menu Scene

--- Assets/Scripts/StartMenuScripts/StartMenuSceneMangaerScript.cs
+++ Assets/Scripts/StartMenuScripts/StartMenuSceneMangaerScript.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartMenuSceneMangaerScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    public void StartButtonOnClicked()
+    {
+        SceneManager.LoadScene("Level1Scene");
+    }
+
+    public void QuitButtonOnClicked()
+    {
+        Application.Quit();
     }
 }
