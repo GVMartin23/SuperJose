b837ebf11b6c1fae5b67a4b3815cc20b3908e15f
GVMartin23 <MartinGV19@gcc.edu>
2023-10-19 12:25:03 -0400

    Win Game Scene Basics

---
+++ Assets/Scripts/WinGameScripts/WinGameSceneManagerScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGameSceneManagerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void PlayAgainOnClick()
+    {
+        SceneManager.LoadScene("Level1Scene");
+    }
+
+    public void MainMenuOnClick()
+    {
+        SceneManager.LoadScene("StartMenuScene");
+    }
+}
