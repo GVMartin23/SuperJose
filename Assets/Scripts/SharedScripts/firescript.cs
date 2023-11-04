c91b53ca62661ce782d722efef878716ae0309f8
Kesterholtem21 <kesterholtem21@gcc.edu>
2023-11-04 15:26:12 -0400

    ice look bad

---
+++ Assets/Scripts/SharedScripts/firescript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firescript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        collision.rigidbody.AddForce(Vector2.up * 3);
+        
+    }
+}
