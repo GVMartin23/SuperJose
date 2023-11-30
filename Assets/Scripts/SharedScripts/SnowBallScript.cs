3e8534de8c83fdb5898d9686168e6362e3cb0268
Kesterholtem21 <kesterholtem21@gcc.edu>
2023-11-30 14:02:37 -0500

    snowball sad

--- Assets/Scripts/SharedScripts/SnowBallScript.cs
+++ Assets/Scripts/SharedScripts/SnowBallScript.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(ParticleSystem))]
 public class SnowBallScript : MonoBehaviour
 {
-    
+    ParticleSystem _ps;
+    Rigidbody2D _rbody;
     // Start is called before the first frame update
     private void Start()
     {
+        _ps = GetComponent<ParticleSystem>();
+        _rbody = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     private void Update()
     {
+        
 
-       
+
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
