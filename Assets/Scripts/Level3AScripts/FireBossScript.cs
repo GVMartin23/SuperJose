2add5d5d32d654395bad7582f6db48d58d807bbb
GVMartin23 <MartinGV19@gcc.edu>
2023-12-15 15:47:51 -0500

    final push

--- Assets/Scripts/Level3AScripts/FireBossScript.cs
+++ Assets/Scripts/Level3AScripts/FireBossScript.cs
@@ -7,7 +7,6 @@ using System.Runtime.CompilerServices;
 using System.Collections.Specialized;
 
 [RequireComponent(typeof(Rigidbody2D))]
-
 public class FireBossScript : MonoBehaviour
 {
     public float xLeftBound;
@@ -21,33 +20,37 @@ public class FireBossScript : MonoBehaviour
     public GameObject boss;
     public GameObject fireball;
     private Rigidbody2D _rbody;
-    ManagerScript _manager;
+    private ManagerScript _manager;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        //Get references to manger script and rigidbody
         _manager = FindAnyObjectByType<ManagerScript>();
         _rbody = boss.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        //Follow player if out of set range
         var distance = Math.Abs(Vector2.Distance(boss.transform.position, jose.transform.position));
 
-        if ( distance < range && jose.transform.position.x < 8)
+        if (distance < range && jose.transform.position.x < 8)
         {
             Chase();
         }
     }
 
-    void Shoot()
+    private void Shoot()
     {
+        //Create new fireball
         GameObject fireBall = _manager.GetFireball();
         Rigidbody2D _fireBB = fireBall.GetComponent<Rigidbody2D>();
 
         fireBall.transform.position = gameObject.transform.position;
 
+        //Shoot fireball in a cone around the player
         int rand = UnityEngine.Random.Range(0, 5);
         Vector3 aim = jose.transform.position - transform.position;
         aim.x -= rand;
@@ -57,22 +60,23 @@ public class FireBossScript : MonoBehaviour
         fireBall.SetActive(true);
     }
 
-    void Chase()
+    private void Chase()
     {
+        //Randomize movment towards player
         System.Random rand = new System.Random();
 
         float xDiff = jose.transform.position.x - transform.position.x;
         float curY = _rbody.velocity.y;
         float yVel = (float)(rand.NextDouble() * 2 - 1);
 
-        if (boss.transform.position.y > yUpBound)
+        if (boss.transform.position.y > yUpBound) //Move down if too far away
         {
             Vector3 direction = jose.transform.position - transform.position;
             Vector3 normalizedDirection = direction.normalized;
             Vector3 velocity = normalizedDirection * speed;
             _rbody.velocity = velocity;
         }
-        else if (boss.transform.position.y < yLowBound)
+        else if (boss.transform.position.y < yLowBound) //Move up if too close
         {
             Vector2 newVel = new Vector2(xDiff, 1);
             newVel.Normalize();
@@ -80,11 +84,13 @@ public class FireBossScript : MonoBehaviour
         }
         else
         {
-             Vector2 newVel = new Vector2(xDiff, curY + yVel);
-             newVel.Normalize();
-             _rbody.velocity = newVel * speed;
+            //Randomly move in range
+            Vector2 newVel = new Vector2(xDiff, curY + yVel);
+            newVel.Normalize();
+            _rbody.velocity = newVel * speed;
         }
 
+        //Randomly shoot
         int chance = UnityEngine.Random.Range(0, 1000);
         if (chance < 15)
         {
